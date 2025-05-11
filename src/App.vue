1c662f87ed184e29d4388049bd161ff814305591
SRI ADINDA <sriadinda@student.uir.ac.id>
2025-05-11 15:58:13 +0700

    Merespons emit dengan mengambil dari Child ke Parent Compoent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <Mahasiswa mahasiswi="Sri Adinda" /> <br>
-  <Mahasiswa mahasiswi="Indri Reynani"/> <br>
-  <Mahasiswa mahasiswi="Rahma Fatharani" />
+<Mahasiswi @response="(whatever) => seelectedMahasiswi=whatever" mahasiswi="Sri Adinda" /> <br>
+<Mahasiswi @response="(whatever) => seelectedMahasiswi=whatever" mahasiswi="Indri Reynanti" /> <br>
+<Mahasiswi @response="(whatever) => seelectedMahasiswi=whatever" mahasiswi="Rahma fatharani" /> 
+<p>
+  Mahasiswi yang dipilih adalah : {{ seelectedMahasiswi }}  
+</p>
+
 </template>
 
 <script>
-  import Mahasiswa from './components/Mahasiswa.vue';
+  import Mahasiswi from './components/Mahasiswi.vue';
 
   export default {
     components : {
-      Mahasiswa
+      Mahasiswi
+    },
+    data() {
+      return {
+        seelectedMahasiswi : ''
+      }
     }
-  }
+  } 
 </script>
---
+++ src/components/Mahasiswi.vue
@@ -0,0 +1,17 @@
+<template>
+    Halo, ini adalah siswi bernama {{ mahasiswi }} 🚀 <button @click="showData">Show Detail</button><br>
+</template>
+
+<script>
+    export default {
+        props: {
+            mahasiswi : String
+        },
+        emits: ['response'],
+        methods: {
+            showData() {
+                this.$emit('response', this.mahasiswi)
+            }
+        }
+    }
+</script>
